28068b816e95e830d0dff83b99c0bcb0c13e8d32
Azam <you@example.com>
2026-08-14 16:18:03 +0000

    cron fix

--- setup_cron.sh
+++ setup_cron.sh
@@ -8,7 +8,7 @@ MARKER_START="# LINUX-MONITORING-SYSTEM-START"
 MARKER_END="# LINUX-MONITORING-SYSTEM-END"
 
 NEW_CRONS="${MARKER_START}
-*/5 * * * * bash -c 'source ${SCRIPT_DIR}/config.cfg; CPU=\$(top -bn1 | awk \"/Cpu/ {print \$2}\"); MEM=\$(free | awk \"/Mem:/ {print \$3/\$2 * 100.0}\"); DISK=\$(df / | awk "END{print \$5+0}"); LOAD=\$(awk \"{print \$1}\" /proc/loadavg); bash ${SCRIPT_DIR}/alerts.sh \"\$CPU\" \"\$MEM\" \"\$DISK\" \"\$LOAD\"' >> ${SCRIPT_DIR}/logs/alerts.log 2>&1
+*/5 * * * * bash -c 'source ${SCRIPT_DIR}/config.cfg; CPU=\$(top -bn1 | awk \"/Cpu/ {print \\\$2}\"); MEM=\$(free | awk \"/Mem:/ {print \\\$3/\\\$2 * 100.0}\"); DISK=\$(df -P / | awk \"END{print substr(\\\$5,1,length(\\\$5)-1)}\"); LOAD=\$(awk \"{print \\\$1}\" /proc/loadavg); bash ${SCRIPT_DIR}/alerts.sh \"\$CPU\" \"\$MEM\" \"\$DISK\" \"\$LOAD\"' >> ${SCRIPT_DIR}/logs/alerts.log 2>&1
 */5 * * * * bash ${SCRIPT_DIR}/self_heal.sh >> ${SCRIPT_DIR}/logs/self_heal.log 2>&1
 59 23 * * * bash ${SCRIPT_DIR}/report.sh >> ${SCRIPT_DIR}/logs/report.log 2>&1
 0 0 * * * bash ${SCRIPT_DIR}/log_rotation.sh >> ${SCRIPT_DIR}/logs/rotation.log 2>&1
